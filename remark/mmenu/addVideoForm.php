0fd58d80ca52e049d430817ac55d0615d8da9526
Joy <pateljoy130.com>
2019-06-11 12:17:59 +0530

    Joy

--- remark/mmenu/addVideoForm.php
+++ remark/mmenu/addVideoForm.php
@@ -1,6 +1,11 @@
 <?php
 include_once 'header.html';
+?>
+
+<?php
 include_once 'navigation-bar-admin.html';
-include_once 'body-admin.html';
+include_once 'bodyVideoForm.php';
+?>
+<?php
 include_once 'footer.html';
 ?>
